cba26ce1ebbe35e821fe8d549eee4d4fd1b3243b
ZachLondono <zachary.londono5@gmail.com>
2023-07-14 12:31:06 -0400

    Ignoring files that contain api keys

--- Scripts/PublishNuget.ps1
+++
@@ -1,4 +0,0 @@
-$filepath = ".\CADCodeProxy.csproj"
-[xml]$project = get-content $filePath
-$version = $project.Project.PropertyGroup.Version 
-Invoke-Expression "..\Scripts\nuget.exe add '.\\bin\\Release\\net7.0\\publish\\CADCodeProxy.$version.nupkg' -Source 'R:\\Development\\nuget\'"
