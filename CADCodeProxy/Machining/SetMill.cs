112fbd9ca16d2dcf589aef3f39d105e8b045953b
ZachLondono <zachary.londono5@gmail.com>
2023-11-23 22:41:01 -0500

    Added setmill machining operation

---
+++ CADCodeProxy/Machining/SetMill.cs
@@ -0,0 +1,40 @@
+﻿using CADCode;
+using CADCodeProxy.CADCodeProxy;
+using CADCodeProxy.Enums;
+
+namespace CADCodeProxy.Machining;
+
+public class SetMill : IMachiningOperation {
+
+    public required string ToolName { get; set; }
+    public required Point Start { get; set; }
+    public required double StartDepth { get; set; }
+    public required Offset Offset { get; set; } = Offset.Center;
+    public required int SequenceNumber { get; set; } = 0;
+    public required int NumberOfPasses { get; set; } = 0;
+    public required double FeedSpeed { get; set; } = 0;
+    public required double SpindleSpeed { get; set; } = 0;
+
+    void IMachiningOperation.AddToCode(CADCodeCodeClass code) {
+
+        code.RouteSetmill(
+                        (float)Start.X,
+                        (float)Start.Y,
+                        (float)StartDepth,
+                        ToolName,
+                        0f,
+                        Offset.AsCCOffset(),
+                        0f,
+                        RotationTypes.CC_ROTATION_AUTO,
+                        FaceTypes.CC_UPPER_FACE,
+                        (float)FeedSpeed,
+                        0f,
+                        (float)SpindleSpeed,
+                        0f,
+                        "",
+                        SequenceNumber,
+                        NumberOfPasses: NumberOfPasses);
+
+    }
+
+}
