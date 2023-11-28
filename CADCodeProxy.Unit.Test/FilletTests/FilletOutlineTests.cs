20931fc5f7197c21375b08f23f81efff42b9b259
ZachLondono <zachary.londono5@gmail.com>
2023-11-28 17:11:48 -0500

    Added additional tests for token sequences

--- CADCodeProxy.Unit.Test/FilletTests/FilletOutlineTests.cs
+++ CADCodeProxy.Unit.Test/FilletTests/FilletOutlineTests.cs
@@ -1,29 +1,28 @@
-﻿using CADCodeProxy.Enums;
-using CADCodeProxy.Machining;
+﻿using CADCodeProxy.Machining;
 using FluentAssertions;
 
 namespace CADCodeProxy.Unit.Test.FilletTests;
 
-public class FilletSequenceTests {
+public class FilletOutlineTests {
 
     private readonly TokenAccumulator _sut;
 
-    public FilletSequenceTests() {
+    public FilletOutlineTests() {
         _sut = new TokenAccumulator();
     }
 
     [Fact]
-    public void AddToken_ShouldThrow_WhenRoutesDoNotConnect() {
+    public void AddToken_ShouldThrow_WhenSegmentsDoNotConnect() {
 
         // Arrange
-        var route1 = CreateRoute(new(0, 0), new(10, 0));
-        var route2 = CreateRoute(new(15, 0), new(20, 20));
+        var segment1 = CreateSegment(new(0, 0), new(10, 0));
+        var segment2 = CreateSegment(new(15, 0), new(20, 20));
         var fillet = new Fillet() { Radius = 10 };
 
         // Act
-        _sut.AddToken(route1);
+        _sut.AddToken(segment1);
         _sut.AddToken(fillet);
-        var action = () => _sut.AddToken(route2);
+        var action = () => _sut.AddToken(segment2);
 
         // Assert
         action.Should().Throw<InvalidOperationException>();
@@ -34,14 +33,14 @@ public class FilletSequenceTests {
     public void AddToken_ShouldThrow_WhenSequencesDoNotMatch() {
 
         // Arrange
-        var route1 = CreateRoute(new(0, 0), new(10, 0), sequenceNumber: 1);
-        var route2 = CreateRoute(new(10, 0), new(20, 20), sequenceNumber: 2);
+        var segment1 = CreateSegment(new(0, 0), new(10, 0), sequenceNumber: 1);
+        var segment2 = CreateSegment(new(10, 0), new(20, 20), sequenceNumber: 2);
         var fillet = new Fillet() { Radius = 10 };
 
         // Act
-        _sut.AddToken(route1);
+        _sut.AddToken(segment1);
         _sut.AddToken(fillet);
-        var action = () => _sut.AddToken(route2);
+        var action = () => _sut.AddToken(segment2);
 
         // Assert
         action.Should().Throw<InvalidOperationException>();
@@ -52,14 +51,14 @@ public class FilletSequenceTests {
     public void AddToken_ShouldThrow_WhenToolsDoNotMatch() {
 
         // Arrange
-        var route1 = CreateRoute(new(0, 0), new(10, 0), toolName: "Tool A");
-        var route2 = CreateRoute(new(10, 0), new(20, 20), toolName: "Tool B");
+        var segment1 = CreateSegment(new(0, 0), new(10, 0), toolName: "Tool A");
+        var segment2 = CreateSegment(new(10, 0), new(20, 20), toolName: "Tool B");
         var fillet = new Fillet() { Radius = 10 };
 
         // Act
-        _sut.AddToken(route1);
+        _sut.AddToken(segment1);
         _sut.AddToken(fillet);
-        var action = () => _sut.AddToken(route2);
+        var action = () => _sut.AddToken(segment2);
 
         // Assert
         action.Should().Throw<InvalidOperationException>();
@@ -70,14 +69,14 @@ public class FilletSequenceTests {
     public void AddToken_ShouldThrow_WhenPassCountsDoNotMatch() {
 
         // Arrange
-        var route1 = CreateRoute(new(0, 0), new(10, 0), numberOfPasses: 1);
-        var route2 = CreateRoute(new(10, 0), new(20, 20), numberOfPasses: 2);
+        var segment1 = CreateSegment(new(0, 0), new(10, 0), numberOfPasses: 1);
+        var segment2 = CreateSegment(new(10, 0), new(20, 20), numberOfPasses: 2);
         var fillet = new Fillet() { Radius = 10 };
 
         // Act
-        _sut.AddToken(route1);
+        _sut.AddToken(segment1);
         _sut.AddToken(fillet);
-        var action = () => _sut.AddToken(route2);
+        var action = () => _sut.AddToken(segment2);
 
         // Assert
         action.Should().Throw<InvalidOperationException>();
@@ -88,32 +87,14 @@ public class FilletSequenceTests {
     public void AddToken_ShouldThrow_WhenDepthsDoNotMatch() {
 
         // Arrange
-        var route1 = CreateRoute(new(0, 0), new(10, 0), endDepth: 10);
-        var route2 = CreateRoute(new(10, 0), new(20, 20), startDepth: 15);
+        var segment1 = CreateSegment(new(0, 0), new(10, 0), endDepth: 10);
+        var segment2 = CreateSegment(new(10, 0), new(20, 20), startDepth: 15);
         var fillet = new Fillet() { Radius = 10 };
 
         // Act
-        _sut.AddToken(route1);
+        _sut.AddToken(segment1);
         _sut.AddToken(fillet);
-        var action = () => _sut.AddToken(route2);
-
-        // Assert
-        action.Should().Throw<InvalidOperationException>();
-
-    }
-
-    [Fact]
-    public void AddToken_ShouldThrow_WhenOffsetsDoNotMatch() {
-
-        // Arrange
-        var route1 = CreateRoute(new(0, 0), new(10, 0), offset: Offset.Left);
-        var route2 = CreateRoute(new(10, 0), new(20, 20), offset: Offset.Right);
-        var fillet = new Fillet() { Radius = 10 };
-
-        // Act
-        _sut.AddToken(route1);
-        _sut.AddToken(fillet);
-        var action = () => _sut.AddToken(route2);
+        var action = () => _sut.AddToken(segment2);
 
         // Assert
         action.Should().Throw<InvalidOperationException>();
@@ -124,15 +105,15 @@ public class FilletSequenceTests {
     public void GetOperations_ShouldReturnSequenceOfSegments() {
 
         // Arrange
-        var segment1 = CreateRoute(new(25, 0), new(50, 0));
+        var segment1 = CreateSegment(new(25, 0), new(50, 0));
         var fillet1 = new Fillet() { Radius = 5 };
-        var segment2 = CreateRoute(new(50, 0), new(50, 50));
+        var segment2 = CreateSegment(new(50, 0), new(50, 50));
         var fillet2 = new Fillet() { Radius = 5 };
-        var segment3 = CreateRoute(new(50, 50), new(0, 50));
+        var segment3 = CreateSegment(new(50, 50), new(0, 50));
         var fillet3 = new Fillet() { Radius = 5 };
-        var segment4 = CreateRoute(new(0, 50), new(0, 0));
+        var segment4 = CreateSegment(new(0, 50), new(0, 0));
         var fillet4 = new Fillet() { Radius = 5 };
-        var segment5 = CreateRoute(new(0, 0), new(25, 0));
+        var segment5 = CreateSegment(new(0, 0), new(25, 0));
 
         // Act
         _sut.AddToken(segment1);
@@ -147,16 +128,15 @@ public class FilletSequenceTests {
         var operations = _sut.GetMachiningOperations();
 
         // Assert
-        operations[0].Should().BeOfType<SetMill>();
-        operations[1].Should().BeOfType<Route>();
-        operations[2].Should().BeOfType<Arc>();
-        operations[3].Should().BeOfType<Route>();
-        operations[4].Should().BeOfType<Arc>();
-        operations[5].Should().BeOfType<Route>();
-        operations[6].Should().BeOfType<Arc>();
-        operations[7].Should().BeOfType<Route>();
-        operations[8].Should().BeOfType<Arc>();
-        operations[9].Should().BeOfType<Route>();
+        operations[0].Should().BeOfType<OutlineSegment>();
+        operations[1].Should().BeOfType<ArcOutlineSegment>();
+        operations[2].Should().BeOfType<OutlineSegment>();
+        operations[3].Should().BeOfType<ArcOutlineSegment>();
+        operations[4].Should().BeOfType<OutlineSegment>();
+        operations[5].Should().BeOfType<ArcOutlineSegment>();
+        operations[6].Should().BeOfType<OutlineSegment>();
+        operations[7].Should().BeOfType<ArcOutlineSegment>();
+        operations[8].Should().BeOfType<OutlineSegment>();
 
         Point? lastPos = null;
         foreach (var operation in operations) {
@@ -164,9 +144,12 @@ public class FilletSequenceTests {
             Point start;
             Point end;
 
-            if (operation is IRouteSequenceSegment seg) {
+            if (operation is OutlineSegment seg) {
                 start = seg.Start;
                 end = seg.End;
+            } else if (operation is ArcOutlineSegment arc) {
+                start = arc.Start;
+                end = arc.End;
             } else continue;
 
             if (lastPos is not null) {
@@ -183,40 +166,39 @@ public class FilletSequenceTests {
     public void GetOperations_ShouldReturnSameSequence_WhenRunMultipleTimes() {
 
         // Arrange
-        var route1 = CreateRoute(new(151.0625, 0), new(302.125, 0));
+        var segment1 = CreateSegment(new(151.0625, 0), new(302.125, 0));
         var fillet1 = new Fillet() { Radius = 5 };
-        var route2 = CreateRoute(new(302.125, 0), new(302.125, 1521.325));
+        var segment2 = CreateSegment(new(302.125, 0), new(302.125, 1521.325));
         var fillet2 = new Fillet() { Radius = 5 };
-        var route3 = CreateRoute(new(302.125, 1521.325), new(0, 1521.325));
+        var segment3 = CreateSegment(new(302.125, 1521.325), new(0, 1521.325));
         var fillet3 = new Fillet() { Radius = 5 };
-        var route4 = CreateRoute(new(0, 1521.325), new(0, 0));
+        var segment4 = CreateSegment(new(0, 1521.325), new(0, 0));
         var fillet4 = new Fillet() { Radius = 5 };
-        var route5 = CreateRoute(new(0, 0), new(151.0625, 0));
-
+        var segment5 = CreateSegment(new(0, 0), new(151.0625, 0));
 
         // Act
         var accumulator1 = new TokenAccumulator();
-        accumulator1.AddToken(route1);
+        accumulator1.AddToken(segment1);
         accumulator1.AddToken(fillet1);
-        accumulator1.AddToken(route2);
+        accumulator1.AddToken(segment2);
         accumulator1.AddToken(fillet2);
-        accumulator1.AddToken(route3);
+        accumulator1.AddToken(segment3);
         accumulator1.AddToken(fillet3);
-        accumulator1.AddToken(route4);
+        accumulator1.AddToken(segment4);
         accumulator1.AddToken(fillet4);
-        accumulator1.AddToken(route5);
+        accumulator1.AddToken(segment5);
         var operations1 = accumulator1.GetMachiningOperations();
 
         var accumulator2 = new TokenAccumulator();
-        accumulator2.AddToken(route1);
+        accumulator2.AddToken(segment1);
         accumulator2.AddToken(fillet1);
-        accumulator2.AddToken(route2);
+        accumulator2.AddToken(segment2);
         accumulator2.AddToken(fillet2);
-        accumulator2.AddToken(route3);
+        accumulator2.AddToken(segment3);
         accumulator2.AddToken(fillet3);
-        accumulator2.AddToken(route4);
+        accumulator2.AddToken(segment4);
         accumulator2.AddToken(fillet4);
-        accumulator2.AddToken(route5);
+        accumulator2.AddToken(segment5);
         var operations2 = accumulator2.GetMachiningOperations();
 
         // Assert
@@ -224,29 +206,27 @@ public class FilletSequenceTests {
 
     }
 
-    // TODO: Check if CADCode allows two routes with different tool speeds to be linked together
+    // TODO: Check if CADCode allows two outline segments with different tool speeds to be linked together
 
-    public Route CreateRoute(Point start, Point end,
+    public OutlineSegment CreateSegment(Point start, Point end,
                             string toolName = "",
                             double startDepth = 0,
                             double endDepth = 0,
-                            Offset offset = Offset.None,
                             double feedSpeed = 0,
                             double spindleSpeed = 0,
                             int numberOfPasses = 0,
                             int sequenceNumber = 0) {
 
-        return new Route() {
+        return new OutlineSegment() {
             Start = start,
             End = end,
             ToolName = toolName,
             StartDepth = startDepth,
             EndDepth = endDepth,
-            Offset = offset,
             FeedSpeed = feedSpeed,
             SpindleSpeed = spindleSpeed,
             NumberOfPasses = numberOfPasses,
-            SequenceNumber = sequenceNumber
+            SequenceNumber = sequenceNumber,
         };
 
     }
